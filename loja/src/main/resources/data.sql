b0b99650f768a70ff8bd289dfbbda4465731be07
oluizleme <o.luizleme@gmail.com>
2022-08-25 22:08:01 -0300

    chore(sb-loja): Criado o arquivo de inicialização da base de dados

---
+++ loja/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Bola Jabulani', 'Bola de futebol da Copa do Mundo de 2010', 500.0);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Aquaplay','Brinquedo dos anos 90 com água', 250.0);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Cubo Mágico','Quebra-cabeça',25.50);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Tamagotchi','Brinquedo dos anos 90 que emulava um dinossauro que tinha que cuidar', 30.0);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Genius','Brinquedo muito legal', 456.20);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Pense Bem','Brinquedo tipo calculadora',1500.50);
