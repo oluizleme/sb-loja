7412cf6aa4cd16a4e9e0f6c18fbcde745cc73283
oluizleme <o.luizleme@gmail.com>
2022-08-25 22:25:52 -0300

    feat(sb-loja):Implementado metodo de consulta de produto por Id

--- loja/src/main/java/br/com/oluizleme/loja/controller/ProdutoController.java
+++ loja/src/main/java/br/com/oluizleme/loja/controller/ProdutoController.java
@@ -1,13 +1,19 @@
 package br.com.oluizleme.loja.controller;
 
 import br.com.oluizleme.loja.modelo.Produto;
+import br.com.oluizleme.loja.modelo.dto.ProdutoDescricaoDto;
+import br.com.oluizleme.loja.modelo.dto.ProdutoDto;
 import br.com.oluizleme.loja.repositorio.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -17,8 +23,17 @@ public class ProdutoController {
     private ProdutoRepository produtoRepository;
 
     @GetMapping("/")
-    public List<Produto> listarProdutos() {
+    public List<ProdutoDto> listarProdutos() {
         List<Produto> produtos = produtoRepository.findAll();
-        return produtos;
+        return ProdutoDto.converter(produtos);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<ProdutoDescricaoDto> consultarProduto(@PathVariable @NotNull Long codigo) {
+        Optional<Produto> optional = produtoRepository.findById(codigo);
+        if(optional.isPresent()) {
+            return ResponseEntity.ok(new ProdutoDescricaoDto(optional.get()));
+        }
+        return ResponseEntity.notFound().build();
     }
 }
