2e5fcaf3a18417a13300365c2abb6f88148bb488
oluizleme <o.luizleme@gmail.com>
2022-08-27 23:40:58 -0300

    refactor(sb-loja): Alterado os produtos antigos e inclusão dos inserteis de clientes

--- loja/src/main/resources/data.sql
+++ loja/src/main/resources/data.sql
@@ -1,6 +1,19 @@
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Bola Jabulani', 'Bola de futebol da Copa do Mundo de 2010', 500.0);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Aquaplay','Brinquedo dos anos 90 com água', 250.0);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Cubo Mágico','Quebra-cabeça',25.50);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Tamagotchi','Brinquedo dos anos 90 que emulava um dinossauro que tinha que cuidar', 30.0);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Genius','Brinquedo muito legal', 456.20);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Pense Bem','Brinquedo tipo calculadora',1500.50);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Berrante', 'Instrumento para chamar a boiada', 500.0);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Guaiaca Panteneira','Cinta de peão', 180.0);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Pelego Natural','Pelego natural de pele de carneiro',250.50);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Sela Australiana','Sela em couro do crocodilo australiano', 850.0);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Cuia de tereré','Cuia de xifre de marruá', 156.20);
+INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Erva Mate','Erva sabor menta',60.50);
+
+
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(21370937741, 'Joventino', 'Leoncio');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(79208756769, 'Veio', 'do Rio');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(73225716499, 'José', 'Leoncio');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(25731054908, 'José Lucas', 'de Nada');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(26834718745, 'Tadeu', 'Leoncio');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(90848638280, 'Joventino', 'Leoncio Neto');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(59526666631, 'Xeréu Trindade', 'Cramulhão');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(59591587449, 'Alcides', 'da Fivela de Respeito');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(26186516695, 'Tenório', 'Mardito');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(11924377640, 'Tibério', 'Peão');
+INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(70648687414, 'Eugênio', 'Chalaneiro');
