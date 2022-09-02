b202c4ff20eac8d37fefd5d482d1c94c87a54973
oluizleme <o.luizleme@gmail.com>
2022-09-01 22:04:38 -0300

    refactor(sb-loja): Alterada a implementação: Extraido o método para obtenção das chaves de cache e ttl do arquivo de propriedades; Implementada a serialização dos dados para cache em JSON

--- loja/src/main/java/br/com/oluizleme/loja/config/RedisCacheConfiguration.java
+++ loja/src/main/java/br/com/oluizleme/loja/config/RedisCacheConfiguration.java
@@ -1,5 +1,14 @@
 package br.com.oluizleme.loja.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
@@ -11,6 +20,8 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -23,13 +34,7 @@ public class RedisCacheConfiguration extends CachingConfigurerSupport {
     @Value("${cache.prefix}")
     private String prefix;
     @Autowired
-    private CacheConfigurationProperties cacheConfigurationProperties = null;
-
-    private org.springframework.data.redis.cache.RedisCacheConfiguration createCacheConfiguration(long timeoutInSeconds) {
-        return org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofSeconds(timeoutInSeconds))
-                .computePrefixWith(cacheName -> prefix + ":" + cacheName + ":");
-    }
+    private CacheConfigurationProperties cacheConfigurationProperties;
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
@@ -41,17 +46,21 @@ public class RedisCacheConfiguration extends CachingConfigurerSupport {
     @Bean
     public CacheManager cacheManager(LettuceConnectionFactory redisConnectionFactory) {
         Map<String, org.springframework.data.redis.cache.RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
-        if (Objects.nonNull(cacheConfigurationProperties.getCachesTTL())) {
-            for (Map.Entry<String, String> cacheNameAndTimeout : cacheConfigurationProperties.getCachesTTL().entrySet()) {
-                cacheConfigurations.put(cacheNameAndTimeout.getKey(),
-                        createCacheConfiguration(Long.parseLong(cacheNameAndTimeout.getValue())));
+
+        Map<String, Long> caches = obterCachesETTLs();
+
+        if(Objects.nonNull(caches)) {
+            for(Map.Entry<String, Long> entry: caches.entrySet()) {
+                cacheConfigurations.put(entry.getKey(),
+                        createCacheConfiguration(entry.getValue()));
             }
         }
 
         return RedisCacheManager
                 .builder(redisConnectionFactory)
                 .cacheDefaults(createCacheConfiguration(Long.parseLong(cacheConfigurationProperties.getDefaultTTL())))
-                .withInitialCacheConfigurations(cacheConfigurations).build();
+                .withInitialCacheConfigurations(cacheConfigurations)
+                .build();
     }
 
     @Bean
@@ -60,5 +69,40 @@ public class RedisCacheConfiguration extends CachingConfigurerSupport {
         redisStandaloneConfiguration.setHostName(cacheConfigurationProperties.getHost());
         redisStandaloneConfiguration.setPort(Integer.parseInt(cacheConfigurationProperties.getPort()));
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    private org.springframework.data.redis.cache.RedisCacheConfiguration createCacheConfiguration(long timeoutInSeconds) {
+        return org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(timeoutInSeconds))
+                .computePrefixWith(cacheName -> prefix + ":" + cacheName + ":")
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(initJacksonSerializer()));
+    }
+
+    private Jackson2JsonRedisSerializer<Object> initJacksonSerializer() {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
+                new Jackson2JsonRedisSerializer<>(Object.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_ARRAY);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        jackson2JsonRedisSerializer.setObjectMapper(mapper);
+        return jackson2JsonRedisSerializer;
+    }
+
+    private Map<String, Long> obterCachesETTLs() {
+        Map<String, Long> cacheMap = new HashMap<>();
+        if(Objects.nonNull(cacheConfigurationProperties.getCachesTTL())) {
+            for (Map.Entry<String, String> entry: cacheConfigurationProperties.getCachesTTL().entrySet()) {
+                cacheMap.put(entry.getKey(), Long.parseLong(entry.getValue()));
+            }
+        }
+        return cacheMap;
     }
 }
