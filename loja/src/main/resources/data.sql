ed3b562d5fc4d1957036e6cbfee760882ceddd5b
oluizleme <o.luizleme@gmail.com>
2024-10-03 20:07:17 -0300

    feat(main): Ajustado a estrutura para refletir a modelagem

--- loja/src/main/resources/data.sql
+++ loja/src/main/resources/data.sql
@@ -1,19 +1,19 @@
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Desbravando SOLID', 'Práticas avançadas para códigos de qualidade em Java moderno', 104.9);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Apache Kafka e Spring Boot','Comunicação assíncrona entre microsserviços', 74.9);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Back-end Java','Microsserviços, Spring Boot e Kubernetes', 74.9);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Séries temporais com Prophet','Análise e previsão de dados com Python', 850.0);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('NoSQL','Como armazenar os dados de uma aplicação moderna', 84.20);
-INSERT INTO PRODUTO(nome, descricao, valor) VALUES('Git e GitHub','Controlando versões com Git e GitHub',60.50);
+INSERT INTO PRODUTO(nome, descricao, valor, status) VALUES('Desbravando SOLID', 'Práticas avançadas para códigos de qualidade em Java moderno', 104.9, 'NOVO');
+INSERT INTO PRODUTO(nome, descricao, valor, status) VALUES('Apache Kafka e Spring Boot','Comunicação assíncrona entre microsserviços', 74.9, 'NOVO');
+INSERT INTO PRODUTO(nome, descricao, valor, status) VALUES('Back-end Java','Microsserviços, Spring Boot e Kubernetes', 74.9, 'NOVO');
+INSERT INTO PRODUTO(nome, descricao, valor, status) VALUES('Séries temporais com Prophet','Análise e previsão de dados com Python', 850.0, 'NOVO');
+INSERT INTO PRODUTO(nome, descricao, valor, status) VALUES('NoSQL','Como armazenar os dados de uma aplicação moderna', 84.20, 'NOVO');
+INSERT INTO PRODUTO(nome, descricao, valor, status) VALUES('Git e GitHub','Controlando versões com Git e GitHub',60.50, 'USADO');
 
 
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(21370937741, 'Joventino', 'Leoncio');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(79208756769, 'José', 'Rio');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(73225716499, 'José', 'Leoncio');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(25731054908, 'José Lucas', 'de Nada');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(26834718745, 'Tadeu', 'Costa');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(90848638280, 'Fabio', 'Brandão');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(59526666631, 'Xavier', 'Carvalho');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(59591587449, 'Alcides', 'Silva');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(26186516695, 'Rodrigo', 'Carvalho');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(11924377640, 'Leandro', 'Antunes');
-INSERT INTO CLIENTE(cpf, nome, sobrenome) VALUES(70648687414, 'Eugênio', 'Gomes');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(21370937741, 'Joventino', 'Leoncio', 'ATIVO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(79208756769, 'José', 'Rio', 'AGUARDANDO_ATIVACAO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(73225716499, 'José', 'Leoncio', 'ATIVO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(25731054908, 'José Lucas', 'de Nada', 'INATIVO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(26834718745, 'Tadeu', 'Costa', 'BLOQUEADO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(90848638280, 'Fabio', 'Brandão', 'ATIVO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(59526666631, 'Xavier', 'Carvalho', 'ATIVO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(59591587449, 'Alcides', 'Silva', 'ATIVO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(26186516695, 'Rodrigo', 'Carvalho', 'EXCLUIDO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(11924377640, 'Leandro', 'Antunes', 'AGUARDANDO_ATIVACAO');
+INSERT INTO CLIENTE(cpf, nome, sobrenome, status) VALUES(70648687414, 'Eugênio', 'Gomes', 'ATIVO');
